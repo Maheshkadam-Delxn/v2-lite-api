e8727131c53a0a3796f5d57132a3bffb5beb17c8
sarodeshreyashdelxn <sarodeshreyash.delxn@gmail.com>
2025-09-25 12:38:58 +0530

    vendor schema update

--- src/models/vendor.js
+++ src/models/vendor.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 const vendorSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true},
   email: { type: String, required: true, unique: true, lowercase: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // link with auth user
+  //userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // link with auth user
   vendorCode: { type: String, required: true, unique: true },
   taxNo: { type: String, required: true },
   gstinNo: { type: String, required: true},
   vendorType: { type: String, required: true },
   address: { type: String, required: true},
+  password: { type: String, required: true, minlength: 6,},
 
 
   status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending"
